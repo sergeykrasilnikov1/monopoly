e8c417a7facaba5b806f73df305c7a1616632225
sergeykrasilnikov1 <100011841+sergeykrasilnikov1@users.noreply.github.com>
2024-03-01 19:23:07 +1000

    Delete monotest directory

--- monotest/presence/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path, path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/presence/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    path(r'ws/presence/', consumers.PresenceConsumer.as_asgi()),
-    re_path(r'ws/game/presence/(?P<room_name>\w+)/$', consumers.GameConsumer.as_asgi()),
-]
